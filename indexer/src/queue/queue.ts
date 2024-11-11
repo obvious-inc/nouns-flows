e7617e20da0f020c09c6eadda53c7f42e41bc934
prego <pedropregueiro@gmail.com>
2024-11-11 16:59:48 +0100

    enable embeddings

--- indexer/src/queue/queue.ts
+++ indexer/src/queue/queue.ts
@@ -1,9 +1,6 @@
 import { EmbeddingType, JobBody } from "./job"
 
 export async function postToEmbeddingsQueueRequest(payload: JobBody) {
-  // ignore embeddings
-  return null
-
   if (!process.env.EMBEDDINGS_QUEUE_URL) {
     throw new Error("EMBEDDINGS_QUEUE_URL is not defined")
   }
@@ -32,9 +29,6 @@ export async function postToEmbeddingsQueueRequest(payload: JobBody) {
 }
 
 export async function deleteEmbeddingRequest(contentHash: string, type: EmbeddingType) {
-  // ignore embeddings
-  return null
-
   if (!process.env.EMBEDDINGS_QUEUE_URL) {
     throw new Error("EMBEDDINGS_QUEUE_URL is not defined")
   }
